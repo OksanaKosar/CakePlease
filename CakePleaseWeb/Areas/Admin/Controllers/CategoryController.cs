13161001f16ac3a7d814b3c82e2196f4df7bffd8
OksanaKosar <kosar.oksana@comp-sc.if.ua>
2022-11-20 22:04:13 +0200

    Changed Add Area

---
+++ CakePleaseWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using CakePlease.DataAccess.Repository.IRepository;
+using CakePlease.DateAccess;
+using CakePlease.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CakePleaseWeb.Areas.Admin.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        //get
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+        //get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (CoverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(CoverTypeFromDbFirst);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+        //get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (CoverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(CoverTypeFromDbFirst);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
