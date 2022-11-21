58211741e6c163856aef6e864379f33bbbc47581
OksanaKosar <kosar.oksana@comp-sc.if.ua>
2022-11-21 14:34:01 +0200

    Product view model

---
+++ CakePleaseWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,120 @@
+﻿using CakePlease.DataAccess.Repository.IRepository;
+using CakePlease.DateAccess;
+using CakePlease.Models;
+using CakePlease.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace CakePleaseWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objCategoryList = _unitOfWork.Product.GetAll();
+            return View(objCategoryList);
+        }
+
+        
+
+        
+        //get
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+
+                })
+            };
+            
+            if (id == null || id == 0)
+            {
+                ////create
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
+            }
+            else
+            {
+                //update
+            }
+            
+            return View(productVM);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                //ModelState.TryAddModelError("CustomError", "The DisplayOrder cannot exactly math the Name.");
+                ModelState.TryAddModelError("name", "The DisplayOrder cannot exactly math the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+        //get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(c => c.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDbFirst);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.Category.GetFirstOrDefault(c => c.Id == id);
+            //var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
